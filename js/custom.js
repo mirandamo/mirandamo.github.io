30e1c1d2db73015c89602773b02ef3df56b17e9c
Miranda Mo <mirandamh.mo@gmail.com>
2020-10-26 18:02:32 +0800

    portfolio oct

--- js/custom.js
+++ js/custom.js
@@ -92,3 +92,25 @@ function fadeInPage() {
         fader.classList.remove('fade-in');
     })
 };
+
+/** Intersection Observer API */
+/** ===================== */
+const images = document.querySelectorAll('.lazyload');
+const config = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+  };
+function handleIntersection(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.src = entry.target.dataset.src;
+      entry.target.classList.add('loaded')
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersection);
+
+images.forEach(image => observer.observe(image));
